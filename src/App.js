1773cceb76d7bf2ffae7c14b7e1c22396b37e6e9
Nilay Sharma <nilaysharma642@gmail.com>
2022-06-05 21:10:54 +0530

    contacts app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Web3 from 'web3'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [account, setAccount] = useState(); 
+
+  useEffect(() => {
+    async function load() {
+      const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545');
+      const accounts = await web3.eth.requestAccounts();
+      
+      setAccount(accounts[0]);
+    }
+    
+    load();
+   }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Your account is: {account}
     </div>
   );
 }
